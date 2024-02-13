2532310e0a58cab89f009ab2e89d37b53a9625cb
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-14 01:06:07 +0530

    added delete and getSpecificUser controller

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -22,7 +22,7 @@ export const getAllUser = async(req:Request , res:Response)=>{
     })
 }
 export const insertUser = async(req: Request , res:Response)=>{
-    const body = req.body
+    const body:User = req.body
     const user =  await prisma.user.create({
         data : body
     })
@@ -59,3 +59,33 @@ export const updateUser = async(req:Request , res:Response) =>{
         user: updatedUser,
     });
 }
+
+export const deleteUser = async(req:Request , res:Response)=>{
+    const id : number = parseInt(req.params.id)
+    await prisma.user.delete({
+        where:{id:id}
+    })
+    res.status(200).json({
+        msg:"user deleted successfully"
+    })
+}
+export const getSpecificUser = async(req:Request , res:Response)=>{
+    // const id : number = parseInt(req.params.id)
+    const username: string = req.params.username.toString()
+   const user =  await prisma.user.findUnique({
+        where:{username:username},
+        include:{
+            tweets:true
+        }
+    })
+    if(!user){
+        return res.status(404).json({
+            msg:"user not found"
+        })
+    }
+    res.status(200).json({
+        msg:"user found",
+        user:user
+    })
+}
+
