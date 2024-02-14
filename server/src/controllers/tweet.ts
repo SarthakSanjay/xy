d15a6e29cd69edda3214cae3606780089b27ac93
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-14 23:57:47 +0530

    added like tweet controller

--- server/src/controllers/tweet.ts
+++ server/src/controllers/tweet.ts
@@ -35,3 +35,39 @@ export const deleteTweet = async(req:Request , res:Response) =>{
         msg:"tweet deleted"
     })
 }
+
+export const likeTweet = async(req:Request , res:Response)=>{
+    const tweetId :number = parseInt(req.params.id)
+    const userId :number = parseInt(req.params.userId) //change later
+    try {
+        const existingLike = await prisma.like.findUnique({
+            where:{
+                tweetId:tweetId,
+                userId:userId
+            }
+        })
+        if (existingLike) {
+            await prisma.tweet.update({
+                where:{id:tweetId},
+                data: {likes:{decrement:1}}
+            })
+            return res.status(400).json({ msg: 'You have already liked this tweet' });
+        }
+        await prisma.like.create({
+            data:{
+                tweetId:tweetId,
+                userId:userId
+            }
+        })
+
+        await prisma.tweet.update({
+            where:{id:tweetId},
+            data: {likes:{increment:1}}
+        })
+        res.status(200).json({
+            msg:"tweet liked successfully"
+        })
+    } catch (error) {
+        res.status(500).json({msg:"Internal server error"})
+    }
+}
