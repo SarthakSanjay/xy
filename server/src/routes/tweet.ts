cafb7733fb3bd6f64bd5c65f976a4df9c85263cf
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-15 12:44:43 +0530

    added delete route

--- server/src/routes/tweet.ts
+++ server/src/routes/tweet.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { postTweet,getAllTweets, likeTweet } from '../controllers/tweet'
+import { postTweet,getAllTweets, likeTweet, deleteTweet } from '../controllers/tweet'
 const router = express.Router()
 
 router.route('/').post(postTweet)
 router.route('/all').get(getAllTweets)
 router.route('/:userId/:id').put(likeTweet)
+router.route('/:id').delete(deleteTweet)
 export default router
