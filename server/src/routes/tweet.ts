e8aa2a72b435be98371fd484e26235ecfd1e8478
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-17 23:55:03 +0530

    added repost route

--- server/src/routes/tweet.ts
+++ server/src/routes/tweet.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { postTweet,getAllTweets, likeTweet, deleteTweet } from '../controllers/tweet'
+import { postTweet,getAllTweets, likeTweet, deleteTweet, repostTweet } from '../controllers/tweet'
 const router = express.Router()
 
 router.route('/').post(postTweet)
 router.route('/all').get(getAllTweets)
 router.route('/:userId/:id').put(likeTweet)
+router.route('/repost/:userId/:id').put(repostTweet)
 router.route('/:id').delete(deleteTweet)
 export default router
