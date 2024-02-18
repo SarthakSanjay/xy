7f978810d452f762f461ef0503b840e115f891c6
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-18 23:13:28 +0530

    added bookmarks and userLikedTweet routes

--- server/src/routes/tweet.ts
+++ server/src/routes/tweet.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { postTweet,getAllTweets, likeTweet, deleteTweet, repostTweet } from '../controllers/tweet'
+import { postTweet,getAllTweets, likeTweet, deleteTweet, repostTweet, allrepostAndLikes, userLikedTweet } from '../controllers/tweet'
 const router = express.Router()
 
 router.route('/').post(postTweet)
@@ -7,4 +7,6 @@ router.route('/all').get(getAllTweets)
 router.route('/:userId/:id').put(likeTweet)
 router.route('/repost/:userId/:id').put(repostTweet)
 router.route('/:id').delete(deleteTweet)
+router.route('/liked/:userId').get(userLikedTweet)
+router.route('/lr').get(allrepostAndLikes)
 export default router
