2ecda8cef9b9b8e40fe7a320b32467b6098cd62a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-19 23:41:08 +0530

    added login and register routes

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { deleteUser, getAllUser, getSpecificUser, insertUser, updateUser } from "../controllers/user"
+import { deleteUser, getAllUser, getSpecificUser, loginUser, registerUser, updateUser } from "../controllers/user"
 const router = express.Router()
 
 
-router.route("/",).post(insertUser)
+router.route("/",).post(registerUser)
+router.route("/login",).post(loginUser)
 router.route('/update/:id').put(updateUser)
 router.route('/delete/:id').delete(deleteUser)
 router.route('/all').get(getAllUser)
