f7f3634b26bb137beb04cfd5c6b72f7efebc3f25
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-20 22:51:58 +0530

    added auth middlewares to routes

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,13 +1,14 @@
 import express from "express"
 import { deleteUser, getAllUser, getSpecificUser, loginUser, registerUser, updateUser } from "../controllers/user"
+import { authMiddleware } from "../middleware/authMiddleware"
 const router = express.Router()
 
 
 router.route("/",).post(registerUser)
 router.route("/login",).post(loginUser)
-router.route('/update/:id').put(updateUser)
-router.route('/delete/:id').delete(deleteUser)
-router.route('/all').get(getAllUser)
+router.route('/update').put(authMiddleware,updateUser)
+router.route('/delete').delete(authMiddleware,deleteUser)
+router.route('/all').get(authMiddleware,getAllUser)
 router.route('/user/:username').get(getSpecificUser)
 
 export default router
