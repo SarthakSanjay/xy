e23b8b3ce5670e0ab60d2c234d15399820e8f1be
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-22 21:57:07 +0530

    added sidebar component

---
+++ client/src/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import SidebarContent from "./SidebarContent"
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen lg:w-[25.2%] hidden md:block">
+      <SidebarContent />
+    </div>
+  )
+}
+
+export default Sidebar
