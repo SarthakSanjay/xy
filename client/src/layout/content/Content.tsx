97f7bbbc80616a8cb8fa3247f22335c8422b0f4a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-23 23:20:59 +0530

    added backend api request to retrive tweets

--- client/src/layout/content/Content.tsx
+++ client/src/layout/content/Content.tsx
@@ -1,19 +1,37 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Header from "../Header";
 import Post from "./Post";
+import Tweet from "./Tweet";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+export interface tweet {
+      bookmarks: number;
+      commentCount: number;
+      id: number;
+      likes: number;
+      reposts: number;
+      text: string;
+      userId: number;
+      views: number;
+    };
 const Content = () => {
+    const [post , setPost] = useState([])
+
+    useEffect(()=>{
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/all`)
+        .then(res => {
+            console.log(res.data.tweets);
+            setPost(res.data.tweets)
+        })
+    },[])
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
         <Header />
-      {/* {[1,2,3,4,5,6,7,8].map((item)=>{
-                    return <Card className="h-[400px]">{item}</Card>
-                })} */}
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+        <Tweet />
+      {post.map((tweet: tweet)=>{
+                    return <Post key={tweet.id} tweet={tweet} />
+                })}
     </ScrollArea>
   );
 };
