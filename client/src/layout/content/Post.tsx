bbae698577b0760be22d03fa17eab437d5760bde
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-23 23:21:49 +0530

    added types to post component

--- client/src/layout/content/Post.tsx
+++ client/src/layout/content/Post.tsx
@@ -1,30 +1,32 @@
-import UserAvatar from "@/components/UserAvatar"
-import { Card } from "@/components/ui/card"
-import PostActivity from "./PostActivity"
-import { More } from "./More"
+import UserAvatar from "@/components/UserAvatar";
+import { Card } from "@/components/ui/card";
+import PostActivity from "./PostActivity";
+import { More } from "./More";
+import { tweet } from "./Content";
 
-
-const Post = () => {
+interface PostProps{
+    tweet : tweet
+}
+const Post: React.FC<PostProps> = ({ tweet }) => {
   return (
     <Card className="h-max flex py-2 px-3 bg-black rounded-none">
-        
-          <UserAvatar />
-        
-        <div className="h-full w-full">
-                <div className="flex justify-between my-2">
-                   <div className="flex gap-2 mx-2 text-gray-500">
-                        <h1 className="text-white">Sharko</h1>
-                        <h2>username</h2>
-                        .
-                        <h2>time</h2>
-                   </div>
-                   <More />
-                </div>
-                <p className="mx-2">Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, veniam optio placeat esse aspernatur quae rerum obcaecati officiis. Vero, cum? ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quidem repudiandae inventore quaerat! Veniam aspernatur, laudantium doloremque corporis voluptate voluptas cumque est nesciunt eum reiciendis! Voluptatum tempora molestiae sunt iusto?</p>
-               <PostActivity />
-        </div>
-      </Card>
-  )
-}
+      <UserAvatar />
 
-export default Post
+      <div className="h-full w-full">
+        <div className="flex justify-between my-2">
+          <div className="flex gap-2 mx-2 text-gray-500">
+            <h1 className="text-white">{tweet.id}</h1>
+            <h2>{tweet.id}</h2>.<h2>time</h2>
+          </div>
+          <More />
+        </div>
+        <p className="mx-2">
+            {tweet.text}
+        </p>
+        <PostActivity />
+      </div>
+    </Card>
+  );
+};
+
+export default Post;
