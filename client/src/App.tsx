09c5c8f35847f7e9e7e7aa8fc36f900e17a7200e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-26 22:34:05 +0530

    refactored app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Content from './layout/content/Content'
+import  Cookies from 'js-cookie'
 import Sidebar from './layout/sidebar/Sidebar'
+import Auth from './layout/auth/Auth'
+import RightBar from './layout/rightSection/RightBar'
+
 
 function App() {
-
+  const [isLoggedIn , setIsLoggedIn] = useState(true)
+  useEffect(()=>{
+    if(Cookies.get('token')){
+      setIsLoggedIn(true)
+    }
+  },[])
+ 
+  if(!isLoggedIn){
+    return <Auth />
+  }
   return (
       <div className='bg-black h-screen w-screen flex '>
         <Sidebar />
         <Content />
+        <RightBar />
       </div>
     
   )
