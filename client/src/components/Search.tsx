e4dee6efbb90cbe2f55f82e903baef1d455a77d9
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-26 22:29:55 +0530

    added search component

---
+++ client/src/components/Search.tsx
@@ -0,0 +1,5 @@
+import { Input } from "@/components/ui/input"
+
+export function Search() {
+  return <Input type="email" placeholder="Search" className="bg-gray-700 rounded-full focus-visible:ring-transparent" />
+}
