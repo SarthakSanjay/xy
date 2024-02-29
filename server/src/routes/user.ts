9ccdae0dc63b55ff76280d063be34f33f00a9107
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-29 23:18:07 +0530

    added route to get username

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,14 +1,14 @@
 import express from "express"
-import { deleteUser, getAllUser, getSpecificUser, loginUser, registerUser, updateUser } from "../controllers/user"
+import { deleteUser, displayUsername, getAllUser, getSpecificUser, loginUser, registerUser, updateUser } from "../controllers/user"
 import { authMiddleware } from "../middleware/authMiddleware"
 const router = express.Router()
 
 
-router.route("/",).post(registerUser)
+router.route("/register",).post(registerUser)
 router.route("/login",).post(loginUser)
 router.route('/update').put(authMiddleware,updateUser)
 router.route('/delete').delete(authMiddleware,deleteUser)
 router.route('/all').get(authMiddleware,getAllUser)
 router.route('/user/:username').get(getSpecificUser)
-
+router.route('/username').get(authMiddleware,displayUsername)
 export default router
