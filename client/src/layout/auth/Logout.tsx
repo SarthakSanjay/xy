817e2c741ed395de909c12df992773b01f7c60ef
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-29 23:08:55 +0530

    added where to navigate after logout

--- client/src/layout/auth/Logout.tsx
+++ client/src/layout/auth/Logout.tsx
@@ -11,9 +11,10 @@ import {
   } from "@/components/ui/alert-dialog"
   import { Button } from "@/components/ui/button"
 import Cookies from "js-cookie"
-
+import { useNavigate } from "react-router-dom"
   
   export function Logout() {
+    const navigate = useNavigate()
     return (
       <AlertDialog>
         <AlertDialogTrigger asChild>
@@ -27,6 +28,7 @@ import Cookies from "js-cookie"
             <AlertDialogCancel>Cancel</AlertDialogCancel>
             <AlertDialogAction onClick={()=>{
                 Cookies.remove('token')
+                navigate('/auth')
             }}>Logout</AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
