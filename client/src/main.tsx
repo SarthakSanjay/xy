7777d98e4d7da387d8e6e00c12fbc6091786acf5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-29 23:07:41 +0530

    added routing to the app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './components/theme-provider.tsx'
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
+import Auth from './layout/auth/Auth.tsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />
+  },
+  {
+    path:'/auth',
+    element: <Auth />
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-
-    <App />
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
     </ThemeProvider>
-  </React.StrictMode>,
+
 )
