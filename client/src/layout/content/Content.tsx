346ade15f96eb785590599648a82c06d2ffb61c6
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-29 23:10:22 +0530

    modified content component

--- client/src/layout/content/Content.tsx
+++ client/src/layout/content/Content.tsx
@@ -6,32 +6,36 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export interface tweet {
-      bookmarks: number;
-      commentCount: number;
-      id: number;
-      likes: number;
-      reposts: number;
-      text: string;
-      userId: number;
-      views: number;
-    };
+  bookmarks: number;
+  commentCount: number;
+  id:number;
+  likes: number;
+  reposts: number;
+  text: string;
+  user: {
+    username: string;
+    fullname: string;
+  };
+  createOn: string;
+  views: number;
+}
 const Content = () => {
-    const [post , setPost] = useState([])
+  const [post, setPost] = useState([]);
 
-    useEffect(()=>{
-        axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/all`)
-        .then(res => {
-            console.log(res.data.tweets);
-            setPost(res.data.tweets)
-        })
-    },[])
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/all`).then((res) => {
+      console.log(res.data.tweets);
+      setPost(res.data.tweets);
+    });
+  }, []);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-        <Header />
-        <Tweet />
-      {post.map((tweet: tweet)=>{
-                    return <Post key={tweet.id} tweet={tweet} />
-                })}
+      <Header />
+      <Tweet />
+      {post.map((tweet: tweet) => {
+        console.log("tweet", tweet);
+        return <Post key={tweet.id} tweet={tweet} />;
+      })}
     </ScrollArea>
   );
 };
