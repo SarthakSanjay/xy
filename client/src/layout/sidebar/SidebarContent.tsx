43fc8abf6918d440943f31e998805bad9a4e2a26
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-29 23:13:51 +0530

    added icons to the buttons,added profile bar

--- client/src/layout/sidebar/SidebarContent.tsx
+++ client/src/layout/sidebar/SidebarContent.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/Logo";
 import { Button } from "@/components/ui/button";
-
+import ProfileBar from "./ProfileBar";
+import { Home ,Search ,Bell,Mail,Bookmark,User ,CircleEllipsis } from "lucide-react";
 const SidebarContent = () => {
   return (
     <div className="h-screen w-full  flex flex-col items-end justify-center  ">
@@ -9,18 +10,20 @@ const SidebarContent = () => {
       <Logo />
       </div>
       <div className=" flex flex-col items-start  gap-6 px-8">
-        <Button variant={"ghost"}>Home</Button>
-        <Button variant={"ghost"}>Explore</Button>
-        <Button variant={"ghost"}>Notificaiton</Button>
-        <Button variant={"ghost"}>Messages</Button>
-        <Button variant={"ghost"}>Bookmark</Button>
-        <Button variant={"ghost"}>Profile</Button>
-        <Button variant={"ghost"}>More</Button>
+        <Button className="flex justify-start w-3/4 text-lg" variant={"ghost"}><Home /> <span className="ml-2">Home</span></Button>
+        <Button className="flex justify-start w-3/4 text-lg" variant={"ghost"}><Search /> <span className="ml-2">Explore</span></Button>
+        <Button className="flex justify-start w-3/4 text-lg" variant={"ghost"}><Bell /> <span className="ml-2">Notificaiton</span></Button>
+        <Button className="flex justify-start w-3/4 text-lg" variant={"ghost"}><Mail /> <span className="ml-2">Messages</span></Button>
+        <Button className="flex justify-start w-3/4 text-lg" variant={"ghost"}><Bookmark /> <span className="ml-2">Bookmark</span></Button>
+        <Button className="flex justify-start w-3/4 text-lg" variant={"ghost"}><User /> <span className="ml-2">Profile</span></Button>
+        <Button className="flex justify-start w-3/4 text-lg" variant={"ghost"}><CircleEllipsis /> <span className="ml-2">More</span></Button>
 
         <Button className="bg-sky-500 text-white h-12 rounded-full w-56 my-5 text-lg">
           Post
         </Button>
-      </div>
+
+        <ProfileBar />
+      </div>  
     </div>
   );
 };
