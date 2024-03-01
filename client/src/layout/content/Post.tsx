1b921e72bb4e024e09b42971271af57ee85f1380
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-01 23:59:29 +0530

    modified text style

--- client/src/layout/content/Post.tsx
+++ client/src/layout/content/Post.tsx
@@ -9,13 +9,13 @@ interface PostProps{
 }
 const Post: React.FC<PostProps> = ({ tweet }) => {
   return (
-    <Card className="h-max flex py-2 px-3 bg-black rounded-none">
+    <Card className="h-max flex py-2 px-3 dark:bg-black dark:text-white rounded-none">
       <UserAvatar />
 
       <div className="h-full w-full">
         <div className="flex justify-between my-2">
           <div className="flex gap-2 mx-2 text-gray-500">
-            <h1 className="text-white">{tweet.user.fullname}</h1>
+            <h1 className="text-black dark:text-white">{tweet.user.fullname}</h1>
             <h2>{tweet.user.username}</h2>.<h2>{new Date(tweet.createOn).getHours() +"h"}</h2>
           </div>
           <More />
