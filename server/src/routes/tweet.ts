0616549d0e3942e0a26a8a0fb8490fdb007128c5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-04 23:46:25 +0530

    added addComment and getTweetComment routes

--- server/src/routes/tweet.ts
+++ server/src/routes/tweet.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import { postTweet,getAllTweets, likeTweet, deleteTweet, repostTweet, allrepostAndLikes, userLikedTweet, userBookmarkedTweet, bookmarkTweet } from '../controllers/tweet'
+import { postTweet,getAllTweets, likeTweet, deleteTweet, repostTweet, allrepostAndLikes, userLikedTweet, userBookmarkedTweet, bookmarkTweet, addComment, getTweetComment } from '../controllers/tweet'
+import { authMiddleware } from '../middleware/authMiddleware'
 const router = express.Router()
 
 router.route('/:userId').post(postTweet)
@@ -11,4 +12,6 @@ router.route('/liked/:userId').get(userLikedTweet)
 router.route('/bookmark/:userId/:id').put(bookmarkTweet)
 router.route('/bookmarked/:userId').get(userBookmarkedTweet)
 router.route('/lr').get(allrepostAndLikes)
+router.route('/comment/:tweetId').post(authMiddleware,addComment)
+router.route('/comment/all/:tweetId').get(getTweetComment)
 export default router
