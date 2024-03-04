f8b429043da03b1b077dd6878a315917e35a20c2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-04 23:39:57 +0530

    refactored component

--- client/src/layout/content/Tweet.tsx
+++ client/src/layout/content/Tweet.tsx
@@ -21,7 +21,7 @@ const formSchema = z.object({
   text: z.string()
 });
 
-const Tweet = ({setTweet}:any) => {
+const Tweet = ({setTweet, fromComment}:any ) => {
   useEffect(()=>{console.log("useEffect form tweet")},[onSubmit])
   const navigate = useNavigate();
   console.log(Cookies.get("userId"));
@@ -46,7 +46,9 @@ const Tweet = ({setTweet}:any) => {
   }
   return (
     <Form {...form}>
-      <Card className="h-min flex py-2 px-3 dark:bg-black rounded-none">
+      <Card className={`h-min flex  px-3 dark:bg-black rounded-none
+      ${fromComment? 'border-none py-0 my-0':'py-2'}
+      `}>
         <UserAvatar />
         <div className="mx-2 w-full min-h-[140px]  flex flex-col  ">
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -57,7 +59,7 @@ const Tweet = ({setTweet}:any) => {
                 <FormItem>
                   <FormControl>
                     <Textarea
-                      placeholder="What is happening?!"
+                      placeholder={fromComment?"Post your reply":"What is happening?!"}
                       {...field}
                       className="h-full flex-grow text-xl dark:bg-black border-none focus-visible:ring-transparent resize-none"
                     />
@@ -68,7 +70,7 @@ const Tweet = ({setTweet}:any) => {
             />
             <Button
               type="submit"
-              className="w-[100px] relative left-[410px] bg-sky-500 text-white rounded-full text-xl"
+              className={`w-[100px] relative ${fromComment?'left-[370px]':'left-[410px]'} bg-sky-500 text-white rounded-full text-xl`}
               disabled={!form.formState.isDirty}
             >
               Post
