5105d700df347c1b0e867a34ab205e7abd97e808
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-04 23:40:41 +0530

    redesigned component

--- client/src/layout/postOpt/Comment.tsx
+++ client/src/layout/postOpt/Comment.tsx
@@ -12,9 +12,11 @@ import {
 import { Button } from "@/components/ui/button";
 import { LuMessageSquare } from "react-icons/lu";
 import { tweet } from "../content/Content";
-import React from "react";
+import React, { useState } from "react";
 import Post from "../content/Post";
-
+import { boolean } from "zod";
+import Tweet from "../content/Tweet";
+import { X } from "lucide-react";
 interface CommentProp{
     tweet:tweet
 }
@@ -23,11 +25,13 @@ export const Comment : React.FC<CommentProp> = ({tweet}) =>{
     <AlertDialog>
       <AlertDialogTrigger asChild>
         <Button variant={"link2"}>
-          <LuMessageSquare />
+          <LuMessageSquare />{tweet.commentCount === 0 ? '' : tweet.commentCount}
         </Button>
       </AlertDialogTrigger>
-      <AlertDialogContent>
-        <Post tweet={tweet} />
+      <AlertDialogContent className="bg-black top-52 max-w-xl flex flex-col gap-0">
+        <AlertDialogCancel className="relative top-0 left-0 w-10 h-10 p-0 bg-none rounded-full border-none hover:bg-gray-500/30"><X /></AlertDialogCancel>
+        <Post tweet={tweet} fromComment={true} />
+        <Tweet  fromComment={true}/>
       </AlertDialogContent>
     </AlertDialog>
   );
