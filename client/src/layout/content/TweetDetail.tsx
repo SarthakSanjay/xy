956b5f8f0b74ff0f7f611b51925b03d8a25df002
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-07 23:51:29 +0530

    import tweet and comment interface

--- client/src/layout/content/TweetDetail.tsx
+++ client/src/layout/content/TweetDetail.tsx
@@ -3,41 +3,36 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Label } from "@/components/ui/label";
+import Comment from "../comment/Comment";
 import PostedTweet from "./PostedTweet";
-export interface tweet {
-  bookmarks: number;
-  commentCount: number;
-  id:number;
-  likes: number;
-  reposts: number;
-  text: string;
-  user: {
-    username: string;
-    fullname: string;
-  };
-  createOn: string;
-  views: number;
-}
+import {tweet} from '../types/tweet'
+import {comment} from '../types/comment'
+
 const TweetDetail = () => {
     const {tweetId} = useParams()
-    console.log('tweetId',tweetId);
-//   const [comment, setComment] = useState([]);
+  const [comment, setComment] = useState([]);
 const [tweet, setTweet] = useState<tweet>({ bookmarks: 0, commentCount: 0, id: 0, likes: 0, reposts: 0, text: "", user: { username: "", fullname: "" }, createOn: "", views: 0 });
-
-console.log(tweet);
+console.log(comment);
+// console.log(tweet);
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/${tweetId}`).then((res) => {
       setTweet(res.data.tweet);
     //   console.log(res.data.tweet);
+      axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/comment/all/${tweetId}`)
+      .then((res)=>{
+        setComment(res.data.comments)
+      })
     });
   }, []);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
         <Label>Post</Label>
         <PostedTweet tweet={tweet} fromComment={false} />
-      {/* {comment.map((tweet: tweet) => {
-        return <Post key={tweet.id} tweet={tweet} />;
-      })} */}
+        {
+            comment.map((comment:comment) =>{
+                return <Comment key={comment.id} comment={comment} />
+            })
+        }
     </ScrollArea>
   );
 };
