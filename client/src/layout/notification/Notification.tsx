25f089ceddd54babb1ea7e2e11749ad8028dff05
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-09 23:49:18 +0530

    added notification layout component

---
+++ client/src/layout/notification/Notification.tsx
@@ -0,0 +1,13 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+//this is content section
+const Notification = () => {
+
+  return (
+    <ScrollArea className="w-full lg:w-[41.67%] border">
+     Notification
+    </ScrollArea>
+  );
+};
+
+export default Notification;
