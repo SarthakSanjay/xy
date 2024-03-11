5429755c35d7c59c72e544c0ecddec8effdefdd0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-11 22:54:38 +0530

    initialized bookmark componenet

---
+++ client/src/layout/bookmark/Bookmark.tsx
@@ -0,0 +1,13 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+//this is content section
+const Bookmark = () => {
+
+  return (
+    <ScrollArea className="w-full lg:w-[41.67%] border">
+     Bookmark
+    </ScrollArea>
+  );
+};
+
+export default Bookmark;
