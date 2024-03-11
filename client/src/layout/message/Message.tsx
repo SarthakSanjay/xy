119de3bc516c64f15a00ee6d23c8509fb86cf54e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-11 22:55:40 +0530

    initialized message componenet

---
+++ client/src/layout/message/Message.tsx
@@ -0,0 +1,13 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+//this is content section
+const Message = () => {
+
+  return (
+    <ScrollArea className="w-full lg:w-[41.67%] border">
+     Message
+    </ScrollArea>
+  );
+};
+
+export default Message;
