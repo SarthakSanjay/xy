85fa1f2c90a4615cea68e96e012a9509b0ad27db
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-11 22:55:21 +0530

    initialized explore componenet

---
+++ client/src/layout/explore/Explore.tsx
@@ -0,0 +1,13 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+//this is content section
+const Explore = () => {
+
+  return (
+    <ScrollArea className="w-full lg:w-[41.67%] border">
+     Explore
+    </ScrollArea>
+  );
+};
+
+export default Explore;
