17000b98251ee66a6af4e998e96c034f5be595b8
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-11 22:56:02 +0530

    initialized profile componenet

---
+++ client/src/layout/profile/Profile.tsx
@@ -0,0 +1,13 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+//this is content section
+const Profile = () => {
+
+  return (
+    <ScrollArea className="w-full lg:w-[41.67%] border">
+     Profile
+    </ScrollArea>
+  );
+};
+
+export default Profile;
