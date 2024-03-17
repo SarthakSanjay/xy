dc4828674eb416f3dbea71e808d0c4df420a6916
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-17 23:44:41 +0530

    added getChildComment route

--- server/src/routes/comment.ts
+++ server/src/routes/comment.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/authMiddleware'
-import { addCommentToTweet, getCommentsByTweetId } from '../controllers/comment'
+import { addCommentToTweet, getChildComments, getCommentsByTweetId } from '../controllers/comment'
 const router = express.Router()
 
 router.route('/:tweetId').post(authMiddleware,addCommentToTweet)
 router.route('/getCommentByTweetId/:tweetId').get(getCommentsByTweetId)
-
+router.route('/getChildComments/:commentId').get(getChildComments)
 
 export default router
