709f335fbbc34d1701304900866c9e4d24de13de
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-19 22:31:13 +0530

    modified commentDetails component

--- client/src/layout/comment/CommentDetails.tsx
+++ client/src/layout/comment/CommentDetails.tsx
@@ -2,49 +2,25 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { Label } from "@/components/ui/label";
 import Comment from "../comment/Comment";
-import { tweet } from "../types/tweet";
 import { comment } from "../types/comment";
+import ContentHeader from "../content/ContentHeader";
 
-//original tweet detail page
+//original comment detail page
 const CommentDetails = () => {
   const { commentId } = useParams();
   const [comment, setComment] = useState([]);
-  const [tweet, setTweet] = useState<tweet>({
-    bookmarks: 0,
-    commentCount: 0,
-    id: 0,
-    likes: 0,
-    reposts: 0,
-    text: "",
-    user: { username: "", fullname: "" },
-    createOn: "",
-    views: 0,
-  });
 
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_API_BASE_URL}/comment/getChildComments/${commentId}`)
       .then((res) => {
-        // setTweet(res.data.tweet);
         setComment(res.data.comments[0].childComments);
-        console.log(res.data.comments[0].childComments);
-        // axios
-        //   .get(
-        //     `${
-        //       import.meta.env.VITE_API_BASE_URL
-        //     }/tweet/comment/all/${commentId}`
-        //   )
-        //   .then((res) => {
-        //     setComment(res.data.comments);
-        //   });
       });
-  }, []);
+  }, [commentId]);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-      <Label>Post</Label>
-      {/* <Comment comment={tweet} fromComment={true} /> */}
+      <ContentHeader /> 
       {comment.map((comment: comment) => {
         return <Comment key={comment.id} comment={comment} />;
       })}
