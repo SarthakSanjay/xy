9b75e0a5eb54156335559c36468d15aa2c31d51d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-19 23:01:26 +0530

    added back functionality

--- client/src/layout/content/ContentHeader.tsx
+++ client/src/layout/content/ContentHeader.tsx
@@ -1,11 +1,14 @@
 import { Label } from "@/components/ui/label"
 import { ArrowLeft } from "lucide-react"
-
+import {useNavigate} from 'react-router-dom'
 const ContentHeader = () => {
+  const navigate = useNavigate()
   return (
     <Label>
     <div className="w-full h-16 flex justify-start items-center px-3">
-      <button><ArrowLeft /></button>
+      <button onClick={()=>{
+        navigate(-1)
+      }}><ArrowLeft /></button>
       <h1 className="text-xl px-7">Post</h1>
 
     </div>
