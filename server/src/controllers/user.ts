6bddacc9bdee595b62e3829bd07be97ae482821b
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-20 23:49:12 +0530

    added error handlers

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -16,51 +16,66 @@ interface User{
 }
 export const registerUser = async(req: Request , res:Response)=>{
     const {username , fullname , email ,password ,bio ,designation ,links ,location}:User = req.body
-    const user =  await prisma.user.create({
-        data : {username , fullname , email ,password ,bio ,designation ,links ,location}
-    })
-    const token = jwt.sign({email:email,password:password}, process.env.TOKEN_SECRET as jwt.Secret)
-    console.log(token);
-
-    console.log(user);
-    // console.log(user);
-    res.status(201).json({
-        msg:"user inserted/registered",
-        userId:user.id,
-        token
-    })
+ try {
+       const user =  await prisma.user.create({
+           data : {username , fullname , email ,password ,bio ,designation ,links ,location}
+       })
+       const token = jwt.sign({email:email,password:password}, process.env.TOKEN_SECRET as jwt.Secret)
+       console.log(token);
+   
+       console.log(user);
+       // console.log(user);
+       res.status(201).json({
+           msg:"user inserted/registered",
+           userId:user.id,
+           token
+       })
+ } catch (error:any) {
+    console.log(error.message);
+    res.status(500).json({msg:"Internal server error"})
+ }
 }
 export const loginUser = async(req:Request , res:Response)=>{
     const {email ,password}:User = req.body
     const token = jwt.sign({email:email,password:password},process.env.TOKEN_SECRET as jwt.Secret)
-    const user = await prisma.user.findFirst({
-        where: {
-            email: email
-        }
-    })
-    if(user?.password === password){
-        return res.status(200).json({
-            msg:"login success",
-            userId:user.id,
-            token
-        })
-    }
-
-    console.log(user);
-    // console.log(user);
-    res.status(404).json({
-        msg:"not authorized",
-    })
+ try {
+       const user = await prisma.user.findFirst({
+           where: {
+               email: email
+           }
+       })
+       if(user?.password === password){
+           return res.status(200).json({
+               msg:"login success",
+               userId:user.id,
+               token
+           })
+       }
+   
+       console.log(user);
+       // console.log(user);
+       res.status(404).json({
+           msg:"not authorized",
+       })
+ } catch (error:any) {
+    console.log(error.message);
+    res.status(500).json({msg:"Internal server error"})
+ }
 }
 
 
 export const getAllUser = async(req:CustomRequest , res:Response)=>{
-    const users = await prisma.user.findMany()
-    console.log(req.user?.id);
-    res.status(200).json({
-        msg:"success",
-        users:users
-    })
+   try {
+     const users = await prisma.user.findMany()
+     console.log(req.user?.id);
+     res.status(200).json({
+         msg:"success",
+         users:users
+     })
+   } catch (error:any) {
+    console.log(error.message);
+    res.status(500).json({msg:"Internal server error"})
+   }
 }
 
 
@@ -68,63 +83,83 @@ export const updateUser = async(req:CustomRequest , res:Response) =>{
     const field = req.body
     const id :number = req.user?.id
     console.log('field',field , "id",id);
-    const existingUser = await prisma.user.findUnique({
-        where:{
-            id : id
-        }
-    })
-    if(!existingUser){
-        return res.status(404).json({msg:"user not found"})
-    }
-
-    const updatedUser = await prisma.user.update({
-        where:{id:id},
-        data:{
-            ...existingUser,
-            ...field
-        }
-    })
-    res.status(200).json({
-        msg: 'User updated',
-        user: updatedUser,
-    });
+   try {
+     const existingUser = await prisma.user.findUnique({
+         where:{
+             id : id
+         }
+     })
+     if(!existingUser){
+         return res.status(404).json({msg:"user not found"})
+     }
+ 
+     const updatedUser = await prisma.user.update({
+         where:{id:id},
+         data:{
+             ...existingUser,
+             ...field
+         }
+     })
+     res.status(200).json({
+         msg: 'User updated',
+         user: updatedUser,
+     });
+   } catch (error:any) {
+    console.log(error.message);
+    res.status(500).json({msg:"Internal server error"})
+   }
 }
 
 export const deleteUser = async(req:CustomRequest , res:Response)=>{
     const id : number = req.user?.id
-    await prisma.user.delete({
-        where:{id:id}
-    })
-    res.status(200).json({
-        msg:"user deleted successfully"
-    })
+  try {
+      await prisma.user.delete({
+          where:{id:id}
+      })
+      res.status(200).json({
+          msg:"user deleted successfully"
+      })
+  } catch (error:any) {
+    console.log(error.message);
+    res.status(500).json({msg:"Internal server error"})
+  }
 }
 export const getSpecificUser = async(req:Request , res:Response)=>{
     // const id : number = parseInt(req.params.id)
     const username: string = req.params.username.toString()
-   const user =  await prisma.user.findUnique({
-        where:{username:username},
-        include:{
-            tweets:true
-        }
-    })
-    if(!user){
-        return res.status(404).json({
-            msg:"user not found"
-        })
-    }
-    res.status(200).json({
-        msg:"user found",
-        user:user
-    })
+  try {
+     const user =  await prisma.user.findUnique({
+          where:{username:username},
+          include:{
+              tweets:true
+          }
+      })
+      if(!user){
+          return res.status(404).json({
+              msg:"user not found"
+          })
+      }
+      res.status(200).json({
+          msg:"user found",
+          user:user
+      })
+  } catch (error:any) {
+    console.log(error.message);
+    res.status(500).json({msg:"Internal server error"})
+  }
 }
 
 export const displayUsername = async(req:CustomRequest , res:Response)=>{
     const username:string = req.user?.username
-    if(!username){
-        return res.json({})
+    try {
+        if(!username){
+            return res.json({})
+        }
+        res.status(200).json({
+            username
+        })
+    } catch (error:any) {
+        console.log(error.message);
+        res.status(500).json({msg:"Internal server error"}) 
     }
-    res.status(200).json({
-        username
-    })
 }
