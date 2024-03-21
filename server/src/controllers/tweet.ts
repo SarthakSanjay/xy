84be28e1e1def85165788dd817efd50f393598f2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-21 23:52:19 +0530

    refactored

--- server/src/controllers/tweet.ts
+++ server/src/controllers/tweet.ts
@@ -100,68 +100,7 @@ export const deleteTweet = async(req:Request , res:Response) =>{
    }
 }
 
-export const likeTweet = async(req:Request , res:Response)=>{
-    const tweetId :number = parseInt(req.params.id)
-    const userId :number = parseInt(req.params.userId) //change later
-    try {
-        const existingLike = await prisma.like.findUnique({
-            where:{
-                tweetId:tweetId,
-                userId:userId
-            }
-        })
-        if (existingLike) {
-            console.log(existingLike);
-            await prisma.like.delete({
-                where:{
-                    userId:userId,
-                    // tweetId:tweetId
-                }
-            })
-            await prisma.tweet.update({
-                where:{id:tweetId},
-                data: {likes:{decrement:1}}
-            })
-            return res.status(400).json({ msg: 'You have already liked this tweet' });
-        }
-        await prisma.like.create({
-            data:{
-                tweetId:tweetId,
-                userId:userId
-            }
-        })
 
-        await prisma.tweet.update({
-            where:{id:tweetId},
-            data: {likes:{increment:1}}
-        })
-        res.status(200).json({
-            msg:"tweet liked successfully"
-        })
-    } catch (error:any) {
-        console.log(error.message);
-        res.status(500).json({msg:"Internal server error"})
-    }
-}
-export const userLikedTweet = async(req:Request, res:Response)=>{
-    const userId : number = parseInt(req.params.userId)
-   try {
-     const likes = await prisma.like.findMany({
-         where:{
-             userId:userId
-         },
-         include:{tweet:true}
-     })
-     
-     res.status(200).json({
-         msg:'success',
-         likes:likes
-     })
-   } catch (error:any) {
-    console.log(error.message);
-    res.status(500).json({msg:"Internal server error"})
-   }
-}
 export const repostTweet = async(req:Request , res:Response)=>{
     const tweetId :number = parseInt(req.params.id)
     const userId :number = parseInt(req.params.userId) //change later
