6b49ae7669738688b8190e9485f1f44e2ada667c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-21 23:52:32 +0530

    refactored

--- server/src/routes/tweet.ts
+++ server/src/routes/tweet.ts
@@ -1,17 +1,26 @@
-import express from 'express'
-import { postTweet,getAllTweets, likeTweet, deleteTweet, repostTweet, allrepostAndLikes, userLikedTweet, userBookmarkedTweet, bookmarkTweet, addComment, getTweetComment } from '../controllers/tweet'
-import { authMiddleware } from '../middleware/authMiddleware'
-const router = express.Router()
+import express from "express";
+import {
+  postTweet,
+  getAllTweets,
+  deleteTweet,
+  repostTweet,
+  allrepostAndLikes,
+  userBookmarkedTweet,
+  bookmarkTweet,
+  getTweetById,
+} from "../controllers/tweet";
 
-router.route('/:userId').post(postTweet)
-router.route('/all').get(getAllTweets)
-router.route('/:userId/:id').put(likeTweet)
-router.route('/repost/:userId/:id').put(repostTweet)
-router.route('/:id').delete(deleteTweet)
-router.route('/liked/:userId').get(userLikedTweet)
-router.route('/bookmark/:userId/:id').put(bookmarkTweet)
-router.route('/bookmarked/:userId').get(userBookmarkedTweet)
-router.route('/lr').get(allrepostAndLikes)
-router.route('/comment/:tweetId').post(authMiddleware,addComment)
-router.route('/comment/all/:tweetId').get(getTweetComment)
-export default router
+const router = express.Router();
+
+//tweet
+router.route("/:userId").post(postTweet);
+router.route("/all").get(getAllTweets);
+router.route("/:tweetId").get(getTweetById);
+router.route("/repost/:userId/:id").put(repostTweet);
+router.route("/:id").delete(deleteTweet);
+router.route("/bookmark/:userId/:id").put(bookmarkTweet);
+router.route("/bookmarked/:userId").get(userBookmarkedTweet);
+
+router.route("/lr").get(allrepostAndLikes);
+
+export default router;
