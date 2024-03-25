62832479b33a0888744f38138119084b93193048
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-25 23:23:17 +0530

    modified content component

--- client/src/layout/content/Content.tsx
+++ client/src/layout/content/Content.tsx
@@ -1,27 +1,29 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Header from "../Header";
-import Post from "./PostedTweet";
-import Tweet from "./TweetBox";
+import Tweet from "./Tweet";
+import TweetBox from "./TweetBox";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {tweet} from '../types/tweet'
 //this is content section
 const Content = () => {
   const [posts, setPosts] = useState([]);
-  const [tweet , setTweet] = useState<boolean>(false)
+  const [isTweet , setIsTweet] = useState<boolean>(false)
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/all`).then((res) => {
       setPosts(res.data.tweets);
     });
-  }, [tweet]);
+  }, [isTweet]);
+
+  console.log('post form content',posts);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
       <Header />
-      <Tweet setTweet={setTweet} />
+      <TweetBox setIsTweet={setIsTweet} />
       {posts.map((tweet: tweet) => {
-        // console.log('tweet',tweet);
-        return <Post key={tweet.id} tweet={tweet} />;
+        console.log('tweet form content',tweet);
+        return <Tweet key={tweet.id} tweet={tweet} fromComment={false} />;
       })}
     </ScrollArea>
   );
