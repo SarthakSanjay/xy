ed1f733569da56839f65657bfeb2aa4d21fec4cb
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-25 23:24:33 +0530

    modified tweet details component

--- client/src/layout/content/TweetDetail.tsx
+++ client/src/layout/content/TweetDetail.tsx
@@ -2,23 +2,23 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { Label } from "@/components/ui/label";
-import Comment from "../comment/Comment";
-import PostedTweet from "./PostedTweet";
+import Tweet from "./Tweet";
 import {tweet} from '../types/tweet'
-import {comment} from '../types/comment'
+import ContentHeader from "./ContentHeader";
 
+//original tweet detail page
 const TweetDetail = () => {
     const {tweetId} = useParams()
   const [comment, setComment] = useState([]);
-const [tweet, setTweet] = useState<tweet>({ bookmarks: 0, commentCount: 0, id: 0, likes: 0, reposts: 0, text: "", user: { username: "", fullname: "" }, createOn: "", views: 0 });
-console.log(comment);
+const [tweet, setTweet] = useState<tweet>({ bookmarks: 0, _count:{comment:0,childComments:0}, id: 0, likes: 0, reposts: 0, text: "", user: { username: "", fullname: "" }, createOn: "", views: 0 });
+// console.log(comment);
 // console.log(tweet);
   useEffect(() => {
+    console.log('twwweeetIdddd',tweetId);
     axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/${tweetId}`).then((res) => {
       setTweet(res.data.tweet);
     //   console.log(res.data.tweet);
-      axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/comment/all/${tweetId}`)
+      axios.get(`${import.meta.env.VITE_API_BASE_URL}/comment/getCommentByTweetId/${tweetId}`)
       .then((res)=>{
         setComment(res.data.comments)
       })
@@ -26,12 +26,12 @@ console.log(comment);
   }, []);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-        <Label>Post</Label>
-        <PostedTweet tweet={tweet} fromComment={false} />
+       <ContentHeader />
+        <Tweet tweet={tweet} fromComment={false} />
         {
-            comment.map((comment:comment) =>{
-                return <Comment key={comment.id} comment={comment} />
-            })
+          comment.map((tweet:tweet)=>{
+            return <Tweet key={tweet.id} tweet={tweet} />
+          })
         }
     </ScrollArea>
   );
