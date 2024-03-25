108390b4861b75ed3a7f1267135113ec5e5e9d66
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-25 23:23:42 +0530

    modified post activity component

--- client/src/layout/content/PostActivity.tsx
+++ client/src/layout/content/PostActivity.tsx
@@ -1,39 +1,29 @@
-import { Button } from "@/components/ui/button"
-import { TfiLoop } from "react-icons/tfi";
-import { IoMdHeartEmpty } from "react-icons/io";
-import { BiBarChart } from "react-icons/bi";
-import { FiBookmark } from "react-icons/fi";
-import { TbUpload } from "react-icons/tb";
 import { tweet } from "../types/tweet";
-import { CommentBtn } from "../comment/CommentBtn";
+import LikeBtn from "../btns/LikeBtn";
+import CommentBtn from "../btns/CommentBtn";
+import RepostBtn from "../btns/RepostBtn";
+import ViewsBtn from "../btns/ViewsBtn";
+import BookmarkBtn from "../btns/BookmarkBtn";
+import ShareBtn from "../btns/ShareBtn";
 
-interface TweetProp{
-    tweet : tweet
+interface TweetProp {
+  tweet: tweet;
 }
 //original tweet activity
-const PostActivity: React.FC<TweetProp> = ({tweet}) => {
+const PostActivity: React.FC<TweetProp> = ({ tweet }) => {
+    // console.log('tweet from post activity', tweet);
   return (
     <div className="flex w-full justify-between ">
-                    <CommentBtn tweet={tweet} />
-                    <Button className="hover:text-green-500 hover:bg-green-500/30 rounded-full h-10 w-10 p-0" variant={"link2"}>
-                        <TfiLoop />
-                    </Button>
-                    <Button className="hover:text-pink-500 hover:bg-pink-500/30 rounded-full h-10 w-10 p-0" variant={"link2"}>
-                        <IoMdHeartEmpty />
-                    </Button>
-                    <Button className="hover:text-sky-500 hover:bg-sky-500/30 rounded-full h-10 w-10 p-0" variant={"link2"}>
-                        <BiBarChart />
-                    </Button>
-                    <div className="mx-2 ">
-                    <Button className="hover:text-sky-500 hover:bg-sky-500/30 rounded-full h-10 w-10 py-0 px-1" variant={"link2"}>
-                        <FiBookmark />
-                    </Button>
-                    <Button className="hover:text-sky-500 hover:bg-sky-500/30 rounded-full h-10 w-10 py-0 px-1" variant={"link2"}>
-                        <TbUpload />
-                    </Button>
-                    </div>
+      <CommentBtn tweet={tweet} />
+      <RepostBtn />
+      <LikeBtn tweetId={tweet.id} />
+      <ViewsBtn />
+      <div className="mx-2 ">
+        <BookmarkBtn />
+        <ShareBtn />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostActivity
+export default PostActivity;
