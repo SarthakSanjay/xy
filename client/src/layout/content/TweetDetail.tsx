d94fcaf2f9d74e07e66e4fb1b82ff326d29131b0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-26 23:20:54 +0530

    refactor tweet detail component

--- client/src/layout/content/TweetDetail.tsx
+++ client/src/layout/content/TweetDetail.tsx
@@ -3,36 +3,50 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Tweet from "./Tweet";
-import {tweet} from '../types/tweet'
+import { tweet } from "../types/tweet";
 import ContentHeader from "./ContentHeader";
 
 //original tweet detail page
 const TweetDetail = () => {
-    const {tweetId} = useParams()
+  const { tweetId } = useParams();
   const [comment, setComment] = useState([]);
-const [tweet, setTweet] = useState<tweet>({ bookmarks: 0, _count:{comment:0,childComments:0}, id: 0, likes: 0, reposts: 0, text: "", user: { username: "", fullname: "" }, createOn: "", views: 0 });
-// console.log(comment);
-// console.log(tweet);
+  const [tweet, setTweet] = useState<tweet>({
+    bookmarks: 0,
+    _count: { comment: 0, childComments: 0 },
+    id: 0,
+    likes: 0,
+    reposts: 0,
+    text: "",
+    user: { username: "", fullname: "" },
+    createOn: "",
+    views: 0,
+  });
+
   useEffect(() => {
-    console.log('twwweeetIdddd',tweetId);
-    axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/${tweetId}`).then((res) => {
-      setTweet(res.data.tweet);
-    //   console.log(res.data.tweet);
-      axios.get(`${import.meta.env.VITE_API_BASE_URL}/comment/getCommentByTweetId/${tweetId}`)
-      .then((res)=>{
-        setComment(res.data.comments)
-      })
-    });
+    console.log("twwweeetIdddd", tweetId);
+    axios
+      .get(`${import.meta.env.VITE_API_BASE_URL}/tweet/${tweetId}`)
+      .then((res) => {
+        setTweet(res.data.tweet);
+
+        axios
+          .get(
+            `${
+              import.meta.env.VITE_API_BASE_URL
+            }/comment/getCommentByTweetId/${tweetId}`
+          )
+          .then((res) => {
+            setComment(res.data.comments);
+          });
+      });
   }, []);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-       <ContentHeader />
-        <Tweet tweet={tweet} fromComment={false} />
-        {
-          comment.map((tweet:tweet)=>{
-            return <Tweet key={tweet.id} tweet={tweet} />
-          })
-        }
+      <ContentHeader />
+      <Tweet tweet={tweet} fromComment={false} detail={true} />
+      {comment.map((tweet: tweet) => {
+        return <Tweet key={tweet.id} tweet={tweet} isComment={true} />;
+      })}
     </ScrollArea>
   );
 };
