7e21f23ced00b05746cb41a58907831660a76cce
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-26 23:23:56 +0530

    added functionality to check user is loggedIn or not

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,28 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Content from './layout/content/Content'
 import  Cookies from 'js-cookie'
 import Sidebar from './layout/sidebar/Sidebar'
 import Auth from './layout/auth/Auth'
 import RightBar from './layout/rightSection/RightBar'
-
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [isLoggedIn , setIsLoggedIn] = useState(false)
   useEffect(()=>{
-    if(Cookies.get('token')){
+    const token = Cookies.get('token')
+    if(token){
       setIsLoggedIn(true)
+    }else{
+      setIsLoggedIn(false)
     }
   },[])
- 
-  if(!isLoggedIn){
+  if(isLoggedIn === false){
     return <Auth />
   }
   return (
       <div className='dark:bg-black dark:text-white h-screen w-screen flex '>
         <Sidebar />
-        <Content />
+        <Outlet />
         <RightBar />
       </div>
     
