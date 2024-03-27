07831081a164a074da78078c16e362024e870a43
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-27 23:28:32 +0530

    Add Loading component

--- client/src/layout/content/Content.tsx
+++ client/src/layout/content/Content.tsx
@@ -5,14 +5,20 @@ import TweetBox from "./TweetBox";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {tweet} from '../types/tweet'
+import Loading from "@/components/Loading";
+import useLoading from "@/hooks/useLoading";
+
 //this is content section
 const Content = () => {
   const [posts, setPosts] = useState([]);
   const [isTweet , setIsTweet] = useState<boolean>(false)
+  const {isLoading , setLoading} = useLoading()
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/all`).then((res) => {
+      setLoading(true)
       setPosts(res.data.tweets);
+      setLoading(false)
     });
   }, [isTweet]);
 
@@ -21,10 +27,15 @@ const Content = () => {
     <ScrollArea className="w-full lg:w-[41.67%] border">
       <Header />
       <TweetBox setIsTweet={setIsTweet} />
-      {posts.map((tweet: tweet) => {
+
+      {
+      !isLoading ?
+      posts.map((tweet: tweet) => {
         console.log('tweet form content',tweet);
         return <Tweet key={tweet.id} tweet={tweet} fromComment={false} />;
-      })}
+      }) :
+      <Loading />
+      }
     </ScrollArea>
   );
 };
