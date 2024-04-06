d53e74eaf66810ce53aa3eb8a7250826cbf430fe
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-06 23:54:00 +0530

    Refactored content component

--- client/src/layout/content/Content.tsx
+++ client/src/layout/content/Content.tsx
@@ -4,38 +4,46 @@ import Tweet from "./Tweet";
 import TweetBox from "./TweetBox";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {tweet} from '../types/tweet'
+import { tweet } from "../types/tweet";
 import Loading from "@/components/Loading";
 import useLoading from "@/hooks/useLoading";
 
 //this is content section
 const Content = () => {
   const [posts, setPosts] = useState([]);
-  const [isTweet , setIsTweet] = useState<boolean>(false)
-  const {isLoading , setLoading} = useLoading()
+  // const [isTweet , setIsTweet] = useState<boolean>(false)
+  const { isLoading, setLoading } = useLoading();
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_BASE_URL}/tweet/all`).then((res) => {
-      setLoading(true)
+      setLoading(true);
       setPosts(res.data.tweets);
-      setLoading(false)
+      setLoading(false);
     });
-  }, [isTweet]);
+  }, []);
 
-  console.log('post form content',posts);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
       <Header />
-      <TweetBox setIsTweet={setIsTweet} />
-
-      {
-      !isLoading ?
-      posts.map((tweet: tweet) => {
-        console.log('tweet form content',tweet);
-        return <Tweet key={tweet.id} tweet={tweet} fromComment={false} />;
-      }) :
-      <Loading />
-      }
+      <TweetBox
+        //  setIsTweet={setIsTweet}
+        fromContent={true}
+      />
+      {!isLoading ? (
+        posts.map((tweet: tweet) => {
+          //tweet from content
+          return (
+            <Tweet
+              key={tweet.id}
+              tweet={tweet}
+              fromComment={false}
+              isTweet={true}
+            />
+          );
+        })
+      ) : (
+        <Loading />
+      )}
     </ScrollArea>
   );
 };
