2348bb80f4fd5c898efdb48562ead8a74bff9fba
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-06 23:47:55 +0530

    Modified and extend types

--- client/src/layout/btns/CommentBtn.tsx
+++ client/src/layout/btns/CommentBtn.tsx
@@ -4,20 +4,19 @@ import {
     AlertDialogContent,
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog";
-  import { Button } from "@/components/ui/button";
-  import { LuMessageSquare } from "react-icons/lu";
-
-
-  import { X } from "lucide-react";
-import { tweet } from "../types/tweet";
+import { Button } from "@/components/ui/button";
+import { LuMessageSquare } from "react-icons/lu";
+import { X } from "lucide-react";
+import { tweet, TweetBoxTypes } from "../types/tweet";
 import Tweet from "../content/Tweet";
 import TweetBox from "../content/TweetBox";
-  interface CommentBtnProp{
+  interface CommentBtnProp extends TweetBoxTypes{
     tweet:tweet
+    fromComment?:boolean
   }
   //button to add comment to origial post
-  const  CommentBtn :React.FC<CommentBtnProp> = ({tweet}) =>{
-    console.log("comment",tweet.text ,tweet.id ,"count",tweet._count);
+  const  CommentBtn :React.FC<CommentBtnProp> = ({ tweet , isComment , isChildComment, fromComment , isTweet}) =>{
+    // console.log('tweet form comment',tweet);
     return (
       <AlertDialog>
         <AlertDialogTrigger asChild>
@@ -25,14 +24,12 @@ import TweetBox from "../content/TweetBox";
             <LuMessageSquare />
             <span className="text-sm pl-1">{tweet._count?.comment}</span>
             <span className="text-sm pl-1">{tweet._count?.childComments || ""}</span>
-            
-            {/* {tweet._count?.comment  ? tweet._count?.comment : tweet._count?.childComments} */}
           </Button>
         </AlertDialogTrigger>
         <AlertDialogContent className="bg-black top-52 max-w-xl flex flex-col gap-0">
           <AlertDialogCancel className="relative top-0 left-0 w-10 h-10 p-0 bg-none rounded-full border-none hover:bg-gray-500/30"><X /></AlertDialogCancel>
           <Tweet tweet={tweet} fromComment={true} />
-          <TweetBox tweetId={tweet.id}  fromComment={true} isComment={true} />
+          <TweetBox tweetId={tweet.id}  fromComment={fromComment} isComment={isComment} isChildComment={isChildComment} isTweet={isTweet} />
         </AlertDialogContent>
       </AlertDialog>
     );
