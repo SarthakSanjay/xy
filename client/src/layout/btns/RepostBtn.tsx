eaea595fc607edb50db0db9d825f67c6278bf195
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-06 23:54:57 +0530

    Modified Repost btn component

--- client/src/layout/btns/RepostBtn.tsx
+++ client/src/layout/btns/RepostBtn.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { TOKEN } from "@/utils/constant";
+import axios from "axios";
 import { TfiLoop } from "react-icons/tfi";
+interface RepostBtnProp {
+  tweetId: number;
+}
+const RepostBtn: React.FC<RepostBtnProp> = ({ tweetId }) => {
+  const handelRepost = () => {
 
-const RepostBtn = () => {
+    axios
+      .put(`${import.meta.env.VITE_API_BASE_URL}/repost/${tweetId}`, null, {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
+      .then(() => {
+        console.log("tweet reposted");
+      });
+  };
   return (
     <Button
+      onClick={handelRepost}
       className="hover:text-green-500 hover:bg-green-500/30 rounded-full h-10 w-10 p-0"
       variant={"link2"}
     >
