3cc0c5a838d98f8df409c56acb98747326155c7b
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-06 23:46:11 +0530

    remove console logs

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,7 +1,6 @@
 import { Loader2 } from "lucide-react";
 
 const Loading = () => {
-  console.log("loading rendered");
   return (
     <div className="w-full flex justify-center">
       <Loader2 className="h-10 w-10 animate-spin text-sky-500" />
