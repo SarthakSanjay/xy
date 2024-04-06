b42c18f2a374ff0d118d563c81c70f026221e80f
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-06 23:52:51 +0530

    Extends post activity prop types

--- client/src/layout/content/PostActivity.tsx
+++ client/src/layout/content/PostActivity.tsx
@@ -1,4 +1,4 @@
-import { tweet } from "../types/tweet";
+import { tweet, TweetBoxTypes } from "../types/tweet";
 import LikeBtn from "../btns/LikeBtn";
 import CommentBtn from "../btns/CommentBtn";
 import RepostBtn from "../btns/RepostBtn";
@@ -6,16 +6,29 @@ import ViewsBtn from "../btns/ViewsBtn";
 import BookmarkBtn from "../btns/BookmarkBtn";
 import ShareBtn from "../btns/ShareBtn";
 
-interface TweetProp {
+interface PostActivityProp extends TweetBoxTypes {
   tweet: tweet;
+  fromComment?: boolean;
 }
+
 //original tweet activity
-const PostActivity: React.FC<TweetProp> = ({ tweet }) => {
-    // console.log('tweet from post activity', tweet);
+const PostActivity: React.FC<PostActivityProp> = ({
+  tweet,
+  isComment,
+  isChildComment,
+  fromComment,
+  isTweet,
+}) => {
   return (
     <div className="flex w-full justify-between ">
-      <CommentBtn tweet={tweet} />
-      <RepostBtn />
+      <CommentBtn
+        isTweet={isTweet}
+        isComment={isComment}
+        isChildComment={isChildComment}
+        tweet={tweet}
+        fromComment={fromComment}
+      />
+      <RepostBtn tweetId={tweet.id} />
       <LikeBtn tweetId={tweet.id} />
       <ViewsBtn />
       <div className="mx-2 ">
