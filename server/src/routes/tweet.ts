d6e421bbe427223b54a3e32bcba93fd6a300ffca
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-08 00:55:38 +0530

    added get route for users tweet

--- server/src/routes/tweet.ts
+++ server/src/routes/tweet.ts
@@ -7,7 +7,9 @@ import {
   userBookmarkedTweet,
   bookmarkTweet,
   getTweetById,
+  getTweetByUserId,
 } from "../controllers/tweet";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
@@ -15,6 +17,7 @@ const router = express.Router();
 router.route("/:userId").post(postTweet);
 router.route("/all").get(getAllTweets);
 router.route("/:tweetId").get(getTweetById);
+router.route("/user/tweets").get(authMiddleware,getTweetByUserId);
 router.route("/:id").delete(deleteTweet);
 router.route("/bookmark/:userId/:id").put(bookmarkTweet);
 router.route("/bookmarked/:userId").get(userBookmarkedTweet);
