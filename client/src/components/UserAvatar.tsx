76ff65d180d35e3d88dac4e3f9e30cb9d1c52267
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-07 23:51:29 +0530

    Modifie user avatar component

--- client/src/components/UserAvatar.tsx
+++ client/src/components/UserAvatar.tsx
@@ -1,8 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-const UserAvatar = () => {
+interface UserAvatarProp {
+  fromProfile?: boolean
+}
+const UserAvatar: React.FC<UserAvatarProp> = ({fromProfile}) => {
   return (
-    <Avatar>
+          <Avatar className={`${fromProfile ? "h-full w-full" :""}`}>
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
