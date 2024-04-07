2d0101e23cf2569017905afb8a4264ab71b8e550
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-07 23:55:25 +0530

    Added header and user profile info to profile component

--- client/src/layout/profile/Profile.tsx
+++ client/src/layout/profile/Profile.tsx
@@ -1,11 +1,22 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
+import ContentHeader from "../content/ContentHeader";
+import UserAvatar from "@/components/UserAvatar";
+import { Button } from "@/components/ui/button";
+import ProfileInfo from "./ProfileInfo";
 
 //this is content section
 const Profile = () => {
-
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-     Profile
+      <ContentHeader text="username" />
+      <div className="h-[280px] w-full border border-white">
+        <div className="h-[200px] w-full bg-gray-600"></div>
+        <div className="h-[120px] w-[120px] relative left-5 bottom-16">
+          <UserAvatar fromProfile={true} />
+        </div>
+        <Button variant={"outline"} className="border-white rounded-full relative left-[77%] bottom-28">Edit Profile</Button>
+      </div>
+      <ProfileInfo />
     </ScrollArea>
   );
 };
