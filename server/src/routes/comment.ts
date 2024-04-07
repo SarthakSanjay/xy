e13c56ac64abba240d5c790d9e85d87faa7f120c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-07 23:52:55 +0530

    added getSpecificComment route

--- server/src/routes/comment.ts
+++ server/src/routes/comment.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/authMiddleware'
-import { addCommentToTweet, getChildComments, getCommentsByTweetId } from '../controllers/comment'
+import { addCommentToTweet, getChildComments, getCommentsByTweetId, getSpecificComment } from '../controllers/comment'
 const router = express.Router()
 
 router.route('/:tweetId').post(authMiddleware,addCommentToTweet)
 router.route('/getCommentByTweetId/:tweetId').get(getCommentsByTweetId)
 router.route('/getChildComments/:commentId').get(getChildComments)
+router.route('/getSpecificComment/:commentId').get(getSpecificComment)
 
 export default router
