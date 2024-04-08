92be1cdc3ac2d69ea15df4872d2ab55b085aae4c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-08 23:19:49 +0530

    Refactored

--- client/src/layout/content/CommentDetail.tsx
+++ client/src/layout/content/CommentDetail.tsx
@@ -8,30 +8,42 @@ import ContentHeader from "./ContentHeader";
 import useLoading from "@/hooks/useLoading";
 import Loading from "@/components/Loading";
 
+export const defaultTweetObj = {
+  bookmarks: 0,
+  _count: { comment: 0, childComments: 0 },
+  id: 0,
+  likes: 0,
+  reposts: 0,
+  text: "",
+  user: { username: "", fullname: "" },
+  createOn: "",
+  views: 0,
+}
 //original tweet detail page
 const CommentDetail = () => {
-    const tweetId = 2
-  const { commentId } = useParams();
-  const [comment, setComment] = useState([]);
-  const {isLoading , setLoading} = useLoading()
-  const [tweet, setTweet] = useState<tweet>({
-    bookmarks: 0,
-    _count: { comment: 0, childComments: 0 },
-    id: 0,
-    likes: 0,
-    reposts: 0,
-    text: "",
-    user: { username: "", fullname: "" },
-    createOn: "",
-    views: 0,
-  });
+  const [tweet, setTweet] = useState<tweet>(defaultTweetObj);
+  const [comment, setComment] = useState<tweet>(defaultTweetObj);
+  const [childComment, setChildComment] = useState([]);
+
+  const { commentId, parentId } = useParams();
+  const { isLoading, setLoading } = useLoading();
 
   useEffect(() => {
     axios
-    .get(`${import.meta.env.VITE_API_BASE_URL}/tweet/${tweetId}`)
-    .then((res) => {
-        setLoading(true)
+      .get(`${import.meta.env.VITE_API_BASE_URL}/tweet/${parentId}`)
+      .then((res) => {
+        setLoading(true);
         setTweet(res.data.tweet);
+        axios
+          .get(
+            `${
+              import.meta.env.VITE_API_BASE_URL
+            }/comment/getSpecificComment/${commentId}`
+          )
+          .then((res) => {
+            setComment(res.data.comment);
+            console.log(res.data.comment);
+          });
         axios
           .get(
             `${
@@ -39,22 +51,31 @@ const CommentDetail = () => {
             }/comment/getChildComments/${commentId}`
           )
           .then((res) => {
-            setComment(res.data.comments[0].childComments);
-            setLoading(false)
+            setChildComment(res.data.comments[0].childComments);
+            setLoading(false);
           });
       });
   }, [commentId]);
-  console.log('comment',comment);
+
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-      <ContentHeader />
-      <Tweet tweet={tweet} fromComment={false} detail={true} />
-      {
-        !isLoading ?
-      comment.map((tweet: tweet) => {
-        return <Tweet key={tweet.id} tweet={tweet} isComment={true} />;
-      }) : <Loading />
-      }
+      <ContentHeader text="Post"/>
+      <Tweet tweet={tweet} fromComment={false} detail={true} isComment={true} />
+      <Tweet tweet={comment} fromComment={false} detail={true} isComment={true} />
+      {!isLoading ? (
+        childComment.map((tweet: tweet) => {
+          return (
+            <Tweet
+              key={tweet.id}
+              tweet={tweet}
+              isChildComment={true}
+              isComment={true}
+            />
+          );
+        })
+      ) : (
+        <Loading />
+      )}
     </ScrollArea>
   );
 };
