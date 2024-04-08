a0eab69822b8b03dafc6fbdf7ed73410732fb371
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-08 23:25:46 +0530

    Refactored , fetch profile data

--- client/src/layout/profile/ProfileInfo.tsx
+++ client/src/layout/profile/ProfileInfo.tsx
@@ -3,21 +3,48 @@ import {
   TypographyMuted,
   TypographyP,
 } from "@/components/typography/Typography";
-import React from "react";
+import { TOKEN } from "@/utils/constant";
+import { convertIntoMonthYear } from "@/utils/date";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+interface profile {
+  id?: number;
+  username?: string;
+  fullname?: string;
+  bio?: string;
+  createOn: string;
+  designation?: string;
+  email?: string;
+  links?: string;
+  location?: string;
+}
 const ProfileInfo = () => {
+  const [profile, setProfile] = useState<profile>({createOn:''});
+  console.log(profile);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_BASE_URL}/profile`, {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
+      .then((res) => {
+        setProfile(res.data.user);
+      });
+  }, []);
   return (
     <div className="w-full px-5 ">
-      <TypographyH4 text={"Username"} />
-      <TypographyMuted text={"@username"} />
-      <TypographyP text={"Fullstack dev"} />
+      <TypographyH4 text={`${profile.fullname}`} />
+      <TypographyMuted text={`@${profile.username}`} />
+      <TypographyP text={`${profile.bio}`} />
       <div className="flex gap-5">
-        <TypographyMuted text="Software developer/Software Engineer/Programmer" />
-        <TypographyMuted text="India" />
+        <TypographyMuted text={`${profile.designation}`} />
+        <TypographyMuted text={`${profile.location}`} />
       </div>
       <div className="flex gap-5">
-        <TypographyMuted text="github.com/sarthaksanjay" />
-        <TypographyMuted text="joined september 2022" />
+        <TypographyMuted text={`${profile.links}`} />
+        <TypographyMuted text={`Joined ${convertIntoMonthYear(profile.createOn)}`} />
       </div>
       <div className="flex gap-5">
         <div className="flex gap-1 items-center">
