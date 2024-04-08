bc9be9bf27d99989d333df5cca987ff137e30c3d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-08 23:14:12 +0530

    Added get comment by userId api route

--- server/src/routes/comment.ts
+++ server/src/routes/comment.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/authMiddleware'
-import { addCommentToTweet, getChildComments, getCommentsByTweetId, getSpecificComment } from '../controllers/comment'
+import { addCommentToTweet, getChildComments, getCommentsByTweetId, getCommentsByUserId, getSpecificComment } from '../controllers/comment'
 const router = express.Router()
 
 router.route('/:tweetId').post(authMiddleware,addCommentToTweet)
 router.route('/getCommentByTweetId/:tweetId').get(getCommentsByTweetId)
 router.route('/getChildComments/:commentId').get(getChildComments)
 router.route('/getSpecificComment/:commentId').get(getSpecificComment)
-
+router.route('/replies').get(authMiddleware,getCommentsByUserId)
 export default router
