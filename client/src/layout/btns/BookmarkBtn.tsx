e5e4bfc7eb2d892aacc307740a692d2e9827037f
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-09 23:55:34 +0530

    Modified bookmark btn to bookmark posts

--- client/src/layout/btns/BookmarkBtn.tsx
+++ client/src/layout/btns/BookmarkBtn.tsx
@@ -1,9 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { TOKEN } from "@/utils/constant";
+import axios from "axios";
 import { FiBookmark } from "react-icons/fi";
 
-const BookmarkBtn = () => {
+interface BookmarkBtnProp {
+  tweetId : number
+}
+const BookmarkBtn : React.FC<BookmarkBtnProp> = ({tweetId}) => {
+  const handleBookmark = () =>{
+    axios.put(`${import.meta.env.VITE_API_BASE_URL}/bookmark/${tweetId}`,null,{
+      headers:{
+        Authorization: `Bearer ${TOKEN}`
+      }
+    }).then(() => alert('bookmarked'))
+  }
   return (
     <Button
+    onClick={handleBookmark}
       className="hover:text-sky-500 hover:bg-sky-500/30 rounded-full h-10 w-10 py-0 px-1"
       variant={"link2"}
     >
