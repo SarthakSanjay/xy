8494c7b873d1c29b43474b3975789b4f53c29bb3
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-13 23:14:46 +0530

    render total post using contextAPI

--- client/src/layout/profile/Profile.tsx
+++ client/src/layout/profile/Profile.tsx
@@ -1,22 +1,25 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import ContentHeader from "../content/ContentHeader";
 import UserAvatar from "@/components/UserAvatar";
-import { Button } from "@/components/ui/button";
 import ProfileInfo from "./ProfileInfo";
 import ProfileNavbar from "./ProfileNavbar";
 import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { stateContext } from "@/App";
+import { EditProfile } from "./EditProfile";
 
 //this is content section
 const Profile = () => {
+  const {total ,text} = useContext(stateContext)
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-      <ContentHeader text="username" />
+      <ContentHeader text="username" total={total} type={text} />
       <div className="h-[280px] w-full border border-white">
         <div className="h-[200px] w-full bg-gray-600"></div>
         <div className="h-[120px] w-[120px] relative left-5 bottom-16">
           <UserAvatar fromProfile={true} />
         </div>
-        <Button variant={"outline"} className="border-white rounded-full relative left-[77%] bottom-28">Edit Profile</Button>
+        <EditProfile />
       </div>
       <ProfileInfo />
       <ProfileNavbar />
