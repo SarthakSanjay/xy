a84451d0acb66694c0d4cab97fbcfe116f0f3827
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-14 23:58:03 +0530

    refactor

---
+++ client/src/layout/sidebar/Username.tsx
@@ -0,0 +1,25 @@
+import UserAvatar from "@/components/UserAvatar"
+import axios from "axios"
+import Cookies from "js-cookie"
+import { useEffect, useState } from "react"
+
+const Username: React.FC = () => {
+    const [username , setUsername] = useState<string>('')
+    useEffect(()=>{
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}/username`,{
+            headers:{
+                Authorization:`Bearer ${Cookies.get('token')}`
+            }
+        }).then((res)=>{
+            setUsername(res.data.username)
+        })
+    },[])
+  return (
+    <div className="h-12 w-1/2 text-white flex items-center justify-between">
+        <UserAvatar />
+        <h1 className="text-gray-500">@{username}</h1>
+    </div>
+  )
+}
+
+export default Username
