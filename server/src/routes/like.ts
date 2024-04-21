dd61d8a51cc9a43b3eb5420204bba1de0ef160b5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-21 23:48:41 +0530

    refactored

--- server/src/routes/like.ts
+++ server/src/routes/like.ts
@@ -1,16 +1,12 @@
 import express from "express";
-import {
-    isLikedTweet,
-  likeTweet,
-  userLikedTweet
-} from "../controllers/like";
+import { isLikedTweet, likeTweet, userLikedTweet } from "../controllers/like";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 //tweet
-router.route("/:userId/:id").put(likeTweet);
-router.route("/likedByUser").get(authMiddleware,userLikedTweet);
-router.route('/isLiked/:tweetId').get(authMiddleware,isLikedTweet)
+router.route("/").put(authMiddleware, likeTweet);
+router.route("/likedByUser").get(authMiddleware, userLikedTweet);
+router.route("/isLiked/:tweetId").get(authMiddleware, isLikedTweet);
 
 export default router;
