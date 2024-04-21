2cf9a721762267288a6dacfa28dfce4632db829c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-21 23:38:09 +0530

    added content header and rendered notification messages

--- client/src/layout/notification/Notification.tsx
+++ client/src/layout/notification/Notification.tsx
@@ -1,11 +1,29 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { TOKEN } from "@/utils/constant";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import NotificationMessage, { Noti } from "./NotificationMessage";
+import ContentHeader from "../content/ContentHeader";
 
 //this is content section
 const Notification = () => {
-
+  const [notifications , setNotifications] = useState([])
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_API_BASE_URL}/noti`,{
+      headers:{
+        Authorization: `Bearer ${TOKEN}`
+      }
+    }).then(res => setNotifications(res.data.latestNoti))
+  },[])
+  console.log(notifications);
   return (
     <ScrollArea className="w-full lg:w-[41.67%] border">
-     Notification
+      <ContentHeader text="Notifications" />
+      {
+        notifications.map((noti :Noti) =>{
+          return <NotificationMessage key={noti.id}  noti={noti}/>
+        })
+      }
     </ScrollArea>
   );
 };
