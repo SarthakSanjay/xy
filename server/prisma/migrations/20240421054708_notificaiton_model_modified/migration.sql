e64a36a5f4a11937c8da2b9c34cb1d5e986a911f
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-21 23:39:15 +0530

    modified notification model

---
+++ server/prisma/migrations/20240421054708_notificaiton_model_modified/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Notification" ADD COLUMN     "tweetId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_tweetId_fkey" FOREIGN KEY ("tweetId") REFERENCES "Tweet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
