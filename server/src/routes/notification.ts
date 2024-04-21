26d418b557c6fe1dc3c8018807d4b0c4f58f88f4
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-21 23:48:53 +0530

    refactored

--- server/src/routes/notification.ts
+++ server/src/routes/notification.ts
@@ -1,11 +1,10 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/authMiddleware';
-import { getAllNotification, getNotification } from '../controllers/notification';
+import { getNotification } from '../controllers/notification';
 
 const router = express.Router();
 
 router.route("/").get(authMiddleware,getNotification);
-router.route("/all").get(authMiddleware,getAllNotification);
 
 
 export default router
