bea72e2f11249c0305c59a603b7985f7de3e66b3
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-21 23:46:35 +0530

    removed unnecessary ctrl-func and modified getNotification

--- server/src/controllers/notification.ts
+++ server/src/controllers/notification.ts
@@ -6,68 +6,29 @@ const prisma = new PrismaClient();
 
 export const getNotification = async(req:CustomRequest ,res:Response) =>{
     const userId : number = req.user?.id
-
-    const notifications = await prisma.notification.findMany({
+    console.log("userId",userId);
+    const noti = await prisma.notification.findMany({
         where:{
-            tweet:{
-                userId:userId
-            }
-        },select:{
+            targetId:userId
+         },
+        select:{
             id:true,
             type:true,
             user:{
                 select:{
-                    id:true,
-                    fullname:true,
-                    username:true
+                    fullname:true
                 }
             },
             tweet:{
                 select:{
-                    id:true,
                     text:true
-                }
-            },
-            createOn:true
-        }
-    })
-
-    res.status(200).json({
-        notifications
-    })
-}
-
-export const getAllNotification = async(req:Request ,res:Response) =>{
-    const noti = await prisma.notification.findMany({
-        select:{
-            id:true,
-            type:true,
-            user:true,
-            targetId:true,
-            target:{
-                select:{
-                    id:true,
-                    fullname:true,
-                    username:true
-                    
-                }
-            },
-            tweet:{
-                select:{
-                    user:{
-                        select:{
-                            id:true,
-                            fullname:true,
-                            username:true
-                        }
-                    }
                 }
             }
         }
     })
-   const message = noti.map(n =>{
-        return `${n.user.fullname} ${n.type} ${n.tweet?.user.fullname || n.target?.fullname }`
-    })
-    console.log(noti);
-    res.status(200).json({message})
+    const latestNoti = noti.reverse()
+    console.log(latestNoti);
+    res.status(200).json({latestNoti})
+
 }
+
