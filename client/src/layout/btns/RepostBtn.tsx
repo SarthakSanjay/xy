527208b6aa840b6d54a55c278264c14b53d4a096
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-25 23:19:10 +0530

    modified repost btn

--- client/src/layout/btns/RepostBtn.tsx
+++ client/src/layout/btns/RepostBtn.tsx
@@ -1,31 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { TOKEN } from "@/utils/constant";
 import axios from "axios";
-import { useState } from "react";
-import { TfiLoop } from "react-icons/tfi";
+import { tweet } from "../types/tweet";
+import { Repeat2 } from "lucide-react";
 interface RepostBtnProp {
-  tweetId: number;
+  tweet: tweet;
 }
-const RepostBtn: React.FC<RepostBtnProp> = ({ tweetId }) => {
-  const [isReposted , setIsReposted] = useState<boolean>(false)
-  // useEffect(()=>{
-  //     axios.get(`${import.meta.env.VITE_API_BASE_URL}/like/isLiked/${tweetId}`,{
-  //       headers:{
-  //         Authorization:`Bearer ${TOKEN}`
-  //       }
-  //     })
-  //     .then((res)=>{
-  //         if(res.data.isLikedTweet){
-  //              setIsReposted(true)
-  //         }else{
-  //             setIsReposted(false)
-  //         }
-  //     })
-  // },[isReposted])
+const RepostBtn: React.FC<RepostBtnProp> = ({ tweet}) => {
   const handelRepost = () => {
-    setIsReposted(!isReposted)
     axios
-      .put(`${import.meta.env.VITE_API_BASE_URL}/repost/${tweetId}`, null, {
+      .put(`${import.meta.env.VITE_API_BASE_URL}/repost/${tweet.id}`, null, {
         headers: {
           Authorization: `Bearer ${TOKEN}`,
         },
@@ -40,7 +24,8 @@ const RepostBtn: React.FC<RepostBtnProp> = ({ tweetId }) => {
       className="hover:text-green-500 hover:bg-green-500/30 rounded-full h-10 w-10 p-0"
       variant={"link2"}
     >
-      <TfiLoop className={`${isReposted ? "text-green-500" : ''}`} />
+      <Repeat2 className={`${tweet.isReposted ? "text-green-500 font-extrabold" : ''}`} />
+       <span className="text-sm pl-1">{tweet._count.repost}</span>
     </Button>
   );
 };
