4c9748426557064f484e5cad0dfc620e7aa2d9c4
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-26 23:34:38 +0530

    added route to check if user reposted tweet

--- client/src/layout/btns/RepostBtn.tsx
+++ client/src/layout/btns/RepostBtn.tsx
@@ -3,10 +3,22 @@ import { TOKEN } from "@/utils/constant";
 import axios from "axios";
 import { tweet } from "../types/tweet";
 import { Repeat2 } from "lucide-react";
+import { useEffect, useState } from "react";
 interface RepostBtnProp {
   tweet: tweet;
 }
 const RepostBtn: React.FC<RepostBtnProp> = ({ tweet}) => {
+  const [reposted ,setReposted] = useState(false)
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_API_BASE_URL}/repost/isReposted/${tweet.id}`,{
+      headers:{
+        Authorization: `Bearer ${TOKEN}`
+      }
+    }).then((res)=>{
+      setReposted(res.data.isRepostedTweet)
+    })
+  },[])
+
   const handelRepost = () => {
     axios
       .put(`${import.meta.env.VITE_API_BASE_URL}/repost/${tweet.id}`, null, {
@@ -24,7 +36,7 @@ const RepostBtn: React.FC<RepostBtnProp> = ({ tweet}) => {
       className="hover:text-green-500 hover:bg-green-500/30 rounded-full h-10 w-10 p-0"
       variant={"link2"}
     >
-      <Repeat2 className={`${tweet.isReposted ? "text-green-500 font-extrabold" : ''}`} />
+      <Repeat2 className={`${reposted ? "text-green-500 font-extrabold" : ''}`} />
        <span className="text-sm pl-1">{tweet._count.repost}</span>
     </Button>
   );
