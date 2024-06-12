0d33f0c088368d73b4da59dfc4d7f269bd87dc1d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-12 23:03:55 +0530

    added isReposted route

--- server/src/routes/repost.ts
+++ server/src/routes/repost.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {repostTweet, totalRepostByTweetId, userRepostedTweets} from "../controllers/repost";
+import {isReposted, repostTweet, totalRepostByTweetId, userRepostedTweets} from "../controllers/repost";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
@@ -8,4 +8,5 @@ const router = express.Router();
 router.route("/:id").put(authMiddleware,repostTweet);
 router.route('/total/:id').get(totalRepostByTweetId)
 router.route('/tweets').get(authMiddleware,userRepostedTweets)
+router.route('/isReposted/:tweetId').get(authMiddleware,isReposted)
 export default router;
